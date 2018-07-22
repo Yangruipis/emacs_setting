09e2942036f10cad62d4807a6be0f829f5ed83af
yangruipis <yangruipis@163.com>
2018-07-22 21:12:43 +0800

    20180722

---
+++ elpa/sql-indent-20170112.1507/sql-indent-autoloads.el
@@ -0,0 +1,22 @@
+;;; sql-indent-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
+
+;;;### (autoloads nil "sql-indent" "sql-indent.el" (0 0 0 0))
+;;; Generated autoloads from sql-indent.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sql-indent" '("sql-indent-")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sql-indent-autoloads.el ends here
---
+++ elpa/sql-indent-20170112.1507/sql-indent-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "sql-indent" "20170112.1507" "indentation of SQL statements" 'nil)
