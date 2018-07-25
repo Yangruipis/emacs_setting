17e25d309d67ff1182d9230491fc60240bac0f3a
yangruipis <yangruipis@163.com>
2018-07-25 09:58:27 +0800

    20180725

--- elpa/adaptive-wrap-0.5.2/adaptive-wrap-autoloads.el
+++ elpa/adaptive-wrap-0.5.2/adaptive-wrap-autoloads.el
@@ -1,10 +1,12 @@
 ;;; adaptive-wrap-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "adaptive-wrap" "adaptive-wrap.el" (23059 41108
-;;;;;;  0 0))
+;;;### (autoloads nil "adaptive-wrap" "adaptive-wrap.el" (0 0 0 0))
 ;;; Generated autoloads from adaptive-wrap.el
 
 (autoload 'adaptive-wrap-prefix-mode "adaptive-wrap" "\
@@ -12,11 +14,14 @@ Wrap the buffer text with adaptive filling.
 
 \(fn &optional ARG)" t nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "adaptive-wrap" '("lookup-key" "adaptive-wrap-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; adaptive-wrap-autoloads.el ends here
