a5a6c77351eadf29f64531fb97a1016f4eb3cf0a
yangruipis <yangruipis@163.com>
2018-09-13 22:58:10 +0800

    20180913

---
+++ elpa/graphql-20180912.31/graphql-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "graphql" "20180912.31" "GraphQL utilities" '((emacs "25")) :commit "c16c8e9a1b99ec802ab78ec44b7fc4f4937ef3e2" :keywords '("hypermedia" "tools" "lisp") :authors '(("Sean Allred" . "code@seanallred.com")) :maintainer '("Sean Allred" . "code@seanallred.com") :url "https://github.com/vermiculus/graphql.el")
