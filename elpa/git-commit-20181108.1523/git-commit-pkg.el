f5a0f9b0878bf65c107243986f77a93bba58f542
yangruipis <yangruipis@163.com>
2018-11-10 13:02:49 +0800

    20181110

---
+++ elpa/git-commit-20181108.1523/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20181108.1523" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "13b32ae1c6f459a140f92dfac8f7e4de9fc1274a" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
